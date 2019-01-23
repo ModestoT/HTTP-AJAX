542e119b7886d41bb016de6364e5a78e515d3c17
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-23 14:40:37 -0600

    Set up state in app component file that sends a get request to the server.js file and sets the friends state equal to the data received from the request

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import FriendsList from './components/FriendsList';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => {
+        this.setState({friends: res.data})
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-
+          <FriendsList friends={this.state.friends}/>
       </div>
     );
   }
